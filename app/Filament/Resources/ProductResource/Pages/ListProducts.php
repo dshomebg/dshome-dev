d4ce9e6aa11dd20a8340cac5ac546c5b2f2f2347
Sevdalin Panev <s.panev@gmail.com>
2025-10-21 20:07:57 +0300

    [Fix6]

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
 {
     protected static string $resource = ProductResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
